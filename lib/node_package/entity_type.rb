74e65f3b5e696c0fff89663fc48e0ecad1dbe9e9
cruzbasco <cruzbasco@hotmail.com>
2014-05-12 10:39:52 -0400

    add entity_type also ignore *.json files

--- lib/node_package/entity_type.rb
+++ lib/node_package/entity_type.rb
@@ -1,7 +1,10 @@
 class EntityType
   
+  attr_reader :attributes
+  
   def initialize (main_attribute)
     @main_attribute = main_attribute
+    @attributes = []
   end
   
   def main_property
@@ -11,5 +14,15 @@ class EntityType
   def main_value
     @main_attribute.value
   end
+  
+  def add_attribute (attribute)
+    @attributes.push(attribute)
+  end
+  
+  def remove_attribute (attribute_id)
+    @attributes.delete_if{|attribute| attribute.object_id == attribute_id}
+  end
+  
+  
   
 end
